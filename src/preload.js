29ff464aa95068f4f6400bdbb9f3669c2354c0b1
Mazaa <mazaa001@doc.gold.ac.uk>
2025-02-27 22:05:03 +0000

    bug fixes with tailwindcss+daisyui not working before + revamped some ui

--- src/preload.js
+++ src/preload.js
@@ -1,12 +1,9 @@
 const { contextBridge,ipcRenderer } = require('electron');
 const activeWin = require('active-win');
 
-
-
 contextBridge.exposeInMainWorld('appTracker', {
     getActiveApplication: async () => {
         const app = await activeWin();
-        console.log(app.owner.path);
         if (app) {
             let icon = await ipcRenderer.invoke('get-app-icon', app.owner.path);  // Extracts icon from .exe
 
